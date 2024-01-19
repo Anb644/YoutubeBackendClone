e42d7fa6f2ff0a5f53448f0bd5395018ced4ec90
Anb644 <anbtechsol@gmail.com>
2024-01-19 17:13:10 +0500

    new connection

--- src/index.js
+++ src/index.js
@@ -4,14 +4,52 @@ import {DB_NAME} from "./constants"
 import { Express } from "express";
 
 const app=express()
-;(async () => {
-    try{
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       app.on("error")
 
-    }catch(error){
-        console.error("ERROR",error)
-        throw error
 
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
     }
-} )()
+})()
+
+*/
